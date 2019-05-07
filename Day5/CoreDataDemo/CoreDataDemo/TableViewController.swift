c66bd27376d839d97531849a80cb9a39139b203d
salma salah mahmoud <40217644+Eng-SalmaSalah@users.noreply.github.com>
2019-05-07 13:39:06 +0200

    updating coredatademo

--- Day5/CoreDataDemo/CoreDataDemo/TableViewController.swift
+++ Day5/CoreDataDemo/CoreDataDemo/TableViewController.swift
@@ -43,6 +43,18 @@ class TableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //3ayz a3rd l 7agat l bdefha f l core data
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managerContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName:"Movie")
+        do{
+            movies = try managerContext.fetch(fetchRequest)
+        }catch{
+            
+        }
+    }
     
     // MARK: - Table view data source
     
@@ -74,17 +86,25 @@ class TableViewController: UITableViewController {
      }
      */
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+         // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
      if editingStyle == .delete {
-     // Delete the row from the data source
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managerContext = appDelegate.persistentContainer.viewContext
+        managerContext.delete(movies[indexPath.row])
+        movies.remove(at: indexPath.row)
+        do{
+            try managerContext.save()
+        }
+        catch{
+            print("error")
+        }
+      //elstr da lazm ykon t7t 
      tableView.deleteRows(at: [indexPath], with: .fade)
      } else if editingStyle == .insert {
      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
      }
      }
-     */
     
     /*
      // Override to support rearranging the table view.
