fb9c8c08c3a23d784039f50b1d99c940b3c54b29
salma salah mahmoud <40217644+Eng-SalmaSalah@users.noreply.github.com>
2019-05-09 15:46:03 +0200

    Day6

---
+++ Day6/StaticTableView/StaticTableView/TableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewController.swift
+//  StaticTableView
+//
+//  Created by JETS Mobile Lab - 8 on 5/9/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
